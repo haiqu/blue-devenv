5be7b9eb8158fbd0ec1176af2268a4e98a9129b6
Rob Judd <judd@ob-wan.com>
2016-10-16 19:27:54 +1000

    New build files so that downloads aren't clobbered. Modified to work in Win32.

--- clean-llvm.sh
+++ clean-llvm.sh
@@ -16,19 +16,18 @@
 #   limitations under the License.
 #*******************************************************************************
 INSTALL_DIR=$PWD/clang-arm-fropi
-# Modify according to the cores you want to use 
-PARALLELISM=5
-mkdir llvm-build
-cd llvm-build
-mkdir x86
-mkdir arm
-cd x86
-cmake -DLLVM_PREFIX="llvm-" -DLLVM_TARGETS_TO_BUILD="X86" -DLLVM_BUILD_EXAMPLES=0 -DLLVM_BUILD_TESTS=0 -DLLVM_INCLUDE_TESTS=0 -DLLVM_BUILD_DOCS=0 -DBUILD_SHARED_LIBS=0 -DCMAKE_INSTALL_PREFIX="" -DCMAKE_BUILD_TYPE=Release ../../llvm
-make -j$PARALLELISM
-cd ..
-cd arm
-CC=$PWD/../x86/bin/clang cmake -DLLVM_PREFIX="llvm-" -DLLVM_TARGETS_TO_BUILD="ARM" -DLLVM_BUILD_EXAMPLES=0 -DLLVM_BUILD_TESTS=0 -DLLVM_INCLUDE_TESTS=0 -DLLVM_BUILD_DOCS=0 -DBUILD_SHARED_LIBS=0 -DCMAKE_INSTALL_PREFIX="" -DCMAKE_BUILD_TYPE=Release ../../llvm
-make -j$PARALLELISM
-mkdir $INSTALL_DIR
-cmake -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" -DCMAKE_INSTALL_DO_STRIP=1 -P cmake_install.cmake
+# Linux
+rm -f $INSTALL_DIR/lib/*.a
+# Windows
+rm -f $INSTALL_DIR/lib/*.lib
+rm -f $INSTALL_DIR/bin/llvm-*
+rm -f $INSTALL_DIR/lib/clang/*/lib/linux/libclang_rt*
+rm -fr $INSTALL_DIR/include
+find $INSTALL_DIR/bin -not -name "clang*" -exec rm -f {} \;
+rm $INSTALL_DIR/bin/clang-check $INSTALL_DIR/bin/clang-format
+#Dafuq?
+#cd ../..
+rm -rf llvm-build
+#For fuck's sake don't delete this!
+#rm -rf llvm
 
